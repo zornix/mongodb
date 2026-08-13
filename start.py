60750a1eae00b7a78d1f9fe5bf8166589033c38e
Maksim Shikhaleev <shikhaleevmd@gmail.com>
2026-08-13 15:02:55 -0700

    md file

---
+++ start.py
@@ -0,0 +1,14 @@
+from pymongo import MongoClient
+from pymongo.server_api import ServerApi
+
+uri = "mongodb+srv://<db_username>:BbrF7GOfNQzwmFgh@mongodb-hack.ys30sja.mongodb.net/?appName=MongoDB-HACK"
+
+# Create a new client and connect to the server
+client = MongoClient(uri, server_api=ServerApi('1'))
+
+# Send a ping to confirm a successful connection
+try:
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
